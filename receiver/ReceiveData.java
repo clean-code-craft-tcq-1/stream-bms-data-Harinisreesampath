6b541b5112796cbb9cccb69ae050fd92208d0cd0
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-06-26 18:41:25 +0530

    Update ReceiveData.java

--- receiver/ReceiveData.java
+++ receiver/ReceiveData.java
@@ -1,32 +1,36 @@
 package receiver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class ReceiveData {
+	Map<String, List<Double>> dataMap = new HashMap<>();
 
-	public static void main(String arg[]) throws IOException 
-	{
-		try
-		{
-			Scanner sc = new Scanner(System.in);
-			String incomingData = "";
-			//read data from console as a string
-			while (sc.hasNext()) {
-				incomingData = incomingData.concat(sc.nextLine());
+	public static void main(String args[]) throws IOException {
+		int incomingCount = 14;
+		String incomingData = "";
+		int count = 0;
+		try (BufferedReader lineReader = new BufferedReader(new InputStreamReader(System.in))) {
+			while (true) {
+				String line = lineReader.readLine();
+				incomingData = incomingData.concat(line);
+				count++;
+				if (incomingCount == count)
+					break;
+				
 			}
-			sc.close();
-			
-			PrepareMap prepareMap = new PrepareMap();
-			Map<String, List<Double>> map = prepareMap.prepareDataMap(incomingData);
-			System.out.println(map.get("Temperature"));
-			ReceiverAction receiver = new ReceiverAction();
-			receiver.performAction(map);
-
-		} catch (Exception e) {
-			System.err.println(e);
 		}
+		PrepareMap prepareMap = new PrepareMap();
+		Map<String, List<Double>> map = prepareMap.prepareDataMap(incomingData);
+		ReceiverAction receiver = new ReceiverAction();
+		receiver.performAction(map);
 	}
+
 }
